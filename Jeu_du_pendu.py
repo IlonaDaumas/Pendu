c8b0136c850dcf4c8333d314ea07df1f0eab30b2
Ilona <ilona.daumas.1@ens.etsmtl.ca>
2025-05-20 00:41:55 +0200

    Fonction qui permet l'affichage du mot au cours du jeu, avec des tirets à la place des lettres qui n'ont pas encore été trouvées.

--- Jeu_du_pendu.py
+++ Jeu_du_pendu.py
@@ -1,4 +1,4 @@
-# Ce script est rempli de fonction permettant à la fin de jouer au jeu du pendu
+# Ce script est rempli de fonctions permettant à la fin de jouer au jeu du pendu
 
 import random
 import os
@@ -31,9 +31,12 @@ def choisir_mot(liste_de_mots):
 
 # Fonction qui enlève les accents du mot choisi
 def enlever_accents(mot) :
+    #chaîne de caractère étant immuable, il faut créer un nouveau mot sans rien auquel on ajoutera les lettres sans accents du mot
     mot_sans_accent = ""
     i = 0
+    # Boucle sur le nombre de lettre du mot
     while i < len(mot) :
+        # Conditions pour enlever les accents des lettres avec
         if mot[i] == 'à' or mot[i] == 'â' or mot[i] == 'ä' :
             mot_sans_accent += 'a'
         elif mot[i] == 'é' or mot[i] == 'è' or mot[i] == 'ê' or mot[i] == 'ë' :
@@ -48,20 +51,37 @@ def enlever_accents(mot) :
             mot_sans_accent += 'c'
         else :
             mot_sans_accent += mot[i]
+        # on ajoute 1 à l'indice i pour parcourir la longueur du mot
         i += 1
     return mot_sans_accent
 
 
 
-#Fonction qui demande une lettre à l'utilisateur
+# Fonction qui demande une lettre à l'utilisateur
 def demander_lettre(lettres_deja_donnees):
     lettre = input('Entrez une lettre : ')
     # Boucle pour s'assurer que la lettre n'a pas été donnée avant
     while lettre in lettres_deja_donnees:
         print('Cette lettre a déjà été donnée.')
         lettre = input('Entrez une nouvelle lettre : ')
+    # on ajoute la nouvelle lettre à la liste des lettres déjà données
     lettres_deja_donnees.extend(lettre)
     return lettres_deja_donnees
 
+# Fonction pour l'affichace caché du mot pendant la partie
+def afficher_mot(mot, liste_lettres_trouvees) :
+    # on crée un mot vide pour l'instant
+    affichage_mot = ""
+    i = 0
+    while i < len(mot) :
+        # si la lettre est trouvée, on l'ajoute à l'affichage
+        if mot[i] in liste_lettres_trouvees :
+            affichage_mot += mot[i]
+        else :
+            # si la lettre n'est pas trouvée, on met un tiret
+            affichage_mot += '_'
+        # on ajoute 1 à l'indice i pour parcourir la longueur du mot
+        i += 1
+    return affichage_mot
 
 
